3035899bf11ae7f2895c7cb91220f106e1edea08
davvore33 <davvore33@gmail.com>
2018-02-11 16:18:13 +0100

    Getting AimDirection

--- Source/BulletfullOfTanks/Tank.h
+++ Source/BulletfullOfTanks/Tank.h
@@ -34,6 +34,7 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
+	UPROPERTY(EditAnyWhere, Category = Firing)
+        float LaunchSpeed = 100000; //Starting value 1000 m/s
 	
 };
--- Source/BulletfullOfTanks/TankAimingComponent.cpp
+++ Source/BulletfullOfTanks/TankAimingComponent.cpp
@@ -1,43 +1,54 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.h"
 
 
 // Sets default values for this component's properties
-UTankAimingComponent::UTankAimingComponent()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+UTankAimingComponent::UTankAimingComponent() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 }
 
 
 // Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
+void UTankAimingComponent::BeginPlay() {
+    Super::BeginPlay();
 
-	// ...
-	
+    // ...
+
 }
 
 
 // Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                         FActorComponentTickFunction *ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+    // ...
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation)
-{
-    auto OurTankName = GetOwner()->GetName();
-    auto HeadLocation = Head->GetComponentLocation();
-    UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName,
-           *HitLocation.ToString(), *HeadLocation.ToString());
+void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed) {
+    auto StartLocation = Head->GetSocketLocation(FName("FiringHole"));
+    auto EndLocation = HitLocation;
+    auto TossSpeed = LaunchSpeed;
+    FVector TossVelocity;
+
+    // Calculate Toss Velocity
+    if (UGameplayStatics::SuggestProjectileVelocity(this, OUT TossVelocity,
+                                                    StartLocation,
+                                                    EndLocation,
+                                                    TossSpeed, false, 0, 0,
+                                                    ESuggestProjVelocityTraceOption::DoNotTrace)) {
+
+        auto AimDirection = TossVelocity.GetSafeNormal();
+
+        UE_LOG(LogTemp, Warning, TEXT("Firing at %s"),
+               *AimDirection.ToString());
+    }
 }
 
 void UTankAimingComponent::SetHead(UStaticMeshComponent *Head) {
